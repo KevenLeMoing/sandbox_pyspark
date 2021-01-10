f4fab486bcb29c34f911b0633a9d90c899761ee9
Kéven Le Moing <kevenlemoing@MBP-de-Keven.lan>
2021-01-10 11:55:17 +0100

    missing data

--- dataframe_basics.py
+++ dataframe_basics.py
@@ -1,6 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructField, StructType, StringType, IntegerType
-from pyspark.sql.functions import avg, stddev, format_number
+from pyspark.sql.functions import avg, stddev, format_number, mean
 
 spark = SparkSession.builder.appName('dataframe_basics').getOrCreate()
 df = spark.read.json('data/people.json')
@@ -41,3 +41,11 @@ df4.select(avg('Sales').alias('average_sales')).show()
 std = df4.select(stddev('Sales').alias('std'))
 std.select(format_number('std', 2).alias('std')).show()
 df4.orderBy('Sales').show()
+
+df5 = spark.read.csv('data/ContainsNull.csv', inferSchema=True, header=True)
+df5.show()
+df5.na.drop(thresh=2).show()
+df5.na.drop(subset=['Sales']).show()
+df5.na.fill(value='no_name', subset=['Name']).show()
+mean_val = df5.select(mean(df5['Sales'])).collect()
+mean_sales = mean_val[0][0]
