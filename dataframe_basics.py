6f6ad459735ac7fb014c47baa42d964641d663f7
Kéven Le Moing <kevenlemoing@MBP-de-Keven.lan>
2021-01-10 10:35:21 +0100

    add new basic operations

--- dataframe_basics.py
+++ dataframe_basics.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructField, StructType, StringType, IntegerType
+from pyspark.sql.functions import avg, stddev, format_number
 
 spark = SparkSession.builder.appName('dataframe_basics').getOrCreate()
 df = spark.read.json('data/people.json')
@@ -14,3 +15,29 @@ struct = StructType(fields=manual_schema)
 df2 = spark.read.json(path='data/people.json', schema=struct)
 df2.printSchema()
 
+print('^^^^^^')
+print(type(df['age']))
+print(type(df.select('age')))
+print(type(df.head(2)[0]))
+print(type())
+print(type(df.select(['age', 'name'])))
+df.withColumn('triple_age', df2['age']*3).show()
+df.withColumnRenamed('age', 'new_age').show()
+df.createOrReplaceTempView('people')
+query = spark.sql('SELECT * FROM people')
+query.show()
+
+df3 = spark.read.csv('data/appl_stock.csv', inferSchema=True, header=True)
+df3.filter((df3['Close'] < 200) & (df3['Open'] > 200)).show()
+result_filter = df3.filter(df3['Low'] == 197.16).collect()
+row = result_filter[0]
+print(row.asDict()['Volume'])
+
+df4 = spark.read.csv('data/sales_info.csv', inferSchema=True, header=True)
+group_data = df4.groupby('Conpany')
+group_data.agg({'Sales': 'max'}).show()
+df4.select(avg('Sales').alias('average_sales')).show()
+
+std = df4.select(stddev('Sales').alias('std'))
+std.select(format_number('std', 2).alias('std')).show()
+df4.orderBy('Sales').show()
